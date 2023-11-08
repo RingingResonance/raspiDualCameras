61703aab252d11abdf08edf2fb51deee36071135
RingingResonance <madeofsolder@gmail.com>
2023-11-07 20:41:11 -0600

    Update netCam.sh
    Fixed mistake in upload.

--- netCam.sh
+++ netCam.sh
@@ -20,7 +20,11 @@ for x in {0..10};
         if [ $i -eq 1 ];then
           echo "Using Video Capture Device for Thermal Field: "
           echo /dev/video$n
-          cvlc v4l2:///dev/video$n --sout-x264-keyint 1 --sout-x264-non-deterministic 1 --sout-transcode-threads 4 --sout '#transcode{vcodec=h264,vb=1200,wi>
+          cvlc v4l2:///dev/video$n \
+                --sout-x264-keyint 1 \
+                --sout-transcode-threads 4 \
+--sout '#transcode{vcodec=h264_v4l2m2m,vb=1000,width=320,height=240,fps=10,scodec=none}:rtp{sdp=rtsp://:9091}' \
+                 :demux=264
           exit 0
         else
           echo "Could not find suitable capture device for Thermal Field."
